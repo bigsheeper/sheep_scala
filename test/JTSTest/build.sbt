128d8aa797353b2fdea7d23fbf431e18be5bdda1
bigsheeper <42060877+bigsheeper@users.noreply.github.com>
2020-06-30 10:07:16 +0800

    add jts test

--- test/JTSTest/build.sbt
+++ test/JTSTest/build.sbt
@@ -1,6 +1,3 @@
-import sbt.Keys.resolvers
-import sbt.Keys.artifact
-
 name := "JTSTest"
 
 version := "0.1"
@@ -10,57 +7,8 @@ scalaVersion := "2.12.11"
 libraryDependencies += "org.locationtech.jts" % "jts-core" % "1.16.1"
 libraryDependencies += "org.wololo" % "jts2geojson" % "0.12.0"
 
-resolvers ++= Seq(
-  "GeoSolutions" at "http://maven.geo-solutions.it/",
-  "GeoTools" at "http://download.osgeo.org/webdav/geotools/"
-)
+resolvers += "Open Source Geospatial Foundation Repository" at "https://repo.osgeo.org/repository/release/"
 libraryDependencies ++= Seq(
-//  "org.geotools" % "gt-swing" % "16.1"
-    "org.geotools" % "gt-main" % "17.0",
-    "org.geotools" % "gt-api" % "17.0"
+  "org.geotools" % "gt-main" % "23.1",
+  "org.geotools" % "gt-referencing" % "23.1",
 )
-
-//resolvers +=
-//  "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-//resolvers += "mvnrepository" at "http://mvnrepository.com/artifact/"
-//libraryDependencies += "org.geotools" % "geotools" % "16.1" pomOnly()
-
-//// https://mvnrepository.com/artifact/org.geotools/geotools
-//resolvers += "mvnrepository" at "http://mvnrepository.com/artifact/"
-////resolvers += "central" at "http://repo1.maven.org/maven2/"
-//resolvers +=
-//libraryDependencies += "org.geotools" % "geotools" % "21.0"
-
-//resolvers ++= Seq(
-//  ("geosolutions" at "http://maven.geo-solutions.it/"),
-//  "Osgeo Repo" at "https://nexus.osgeo.org/repository/Geoserver-releases/",
-//  ("geomajas" at "http://maven.geomajas.org")
-//)
-//libraryDependencies ++= Seq(
-//  "org.geotools" % "gt-swing" % "21.0"
-//)
-
-//resolvers += DefaultMavenRepository
-//resolvers += JavaNet2Repository
-//
-////resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-//resolvers += "Open Source Geospatial Foundation Repository" at "https://download.osgeo.org/webdav/geotools/"
-//allDependencies ++= Seq(
-//  "org.geotools" % "gt-main" % "17.0",
-//  "org.geotools" % "gt-api" % "17.0"
-//)
-//
-//lazy val geotools = "17.0"
-//
-////resolvers += "osgeo" at "http://download.osgeo.org/webdav/geotools"
-////resolvers += "boundless" at "http://repo.boundlessgeo.com/main"
-////resolvers += "imageio" at "http://maven.geo-solutions.it"
-////resolvers += Resolver.mavenLocal
-//
-//allDependencies ++= Seq(
-//  "org.geotools" % "gt-main" % geotools,
-//  "org.geotools" % "gt-arcgrid" % geotools,
-//  "org.geotools" % "gt-process-raster" % geotools)
-//  .map(_.excludeAll(
-//    ExclusionRule("com.vividsolutions", "jts")
-//  ))
--- test/JTSTest/src/main/scala/TestJavaJTS.java
+++ test/JTSTest/src/main/scala/TestJavaJTS.java
@@ -1,24 +1,61 @@
+import org.geotools.geometry.GeometryBuilder;
+import org.geotools.geometry.iso.text.WKTParser;
+import org.geotools.geometry.jts.*;
 import org.locationtech.jts.geom.*;
-import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.geom.util.AffineTransformation;
+import org.locationtech.jts.io.*;
+import org.locationtech.jts.io.WKBReader;
 import org.locationtech.jts.operation.IsSimpleOp;
 import org.locationtech.jts.operation.buffer.BufferParameters;
 import org.locationtech.jts.operation.valid.IsValidOp;
 import org.locationtech.jts.precision.GeometryPrecisionReducer;
-import org.locationtech.jts.io.WKTReader;
 import org.locationtech.jts.simplify.TopologyPreservingSimplifier;
-import org.locationtech.jts.io.WKBWriter;
 import org.locationtech.jts.algorithm.distance.DiscreteHausdorffDistance;
-import org.locationtech.jts.io.WKBReader;
-import org.wololo.geojson.Feature;
-import org.wololo.geojson.GeoJSONFactory;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.operation.TransformException;
 import org.wololo.jts2geojson.GeoJSONReader;
-import org.wololo.jts2geojson.GeoJSONWriter;
 import org.locationtech.jts.operation.buffer.OffsetCurveBuilder;
+import scala.Array;
 
-import org.geotools.geometry.jts.JTS;
-import org.geotools.referencing.CRS;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.StrictMath.acos;
+
+//import org.geotools.geometry.jts.JTS;
+//import org.geotools.referencing.CRS;
 
 public class TestJavaJTS {
+    public static void test_geotools_curve() throws ParseException {
+        Geometry fdsgdg = new org.geotools.geometry.jts.WKTReader2().read("CURVEPOLYGON(CIRCULARSTRING(0 0, 4 0, 4 4, 0 4, 0 0))");
+        String dgdfgdfh = new WKTWriter().write(fdsgdg);
+        String fdsgf = "dfsdgf";
+    }
+
+    public static void test_transform() throws FactoryException, ParseException, TransformException {
+        System.setProperty("org.geotools.referencing.forceXY", "true");
+        Geometry geometry = new WKTReader().read("POINT (1 1)");
+        System.out.println(geometry);
+//        CoordinateReferenceSystem sourceCRScode = CRS.decode("EPSG:4326");
+//        CoordinateReferenceSystem targetCRScode = CRS.decode("EPSG:3857");
+//        MathTransform transform = CRS.findMathTransform(CRS.decode("EPSG:4326"), CRS.decode("EPSG:3857"));
+        Geometry res = org.geotools.geometry.jts.JTS.transform(geometry, org.geotools.referencing.CRS.findMathTransform(org.geotools.referencing.CRS.decode("EPSG:4326"), org.geotools.referencing.CRS.decode("EPSG:3857")));
+    }
+
+    public static void test_transform2() {
+        Geometry geometry = null;
+        Geometry res = null;
+        try {
+            System.setProperty("org.geotools.referencing.forceXY", "true");
+            System.out.println(geometry);
+            res = org.geotools.geometry.jts.JTS.transform(geometry, org.geotools.referencing.CRS.findMathTransform(org.geotools.referencing.CRS.decode("EPSG:4326"), org.geotools.referencing.CRS.decode("EPSG:3857")));
+        } catch (FactoryException | TransformException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void test_curve() throws ParseException {
         Geometry geometry = new WKTReader().read("CURVEPOLYGON(CIRCULARSTRING(0 0, 4 0, 4 4, 0 4, 0 0))");
 
@@ -31,6 +68,36 @@ public class TestJavaJTS {
         Coordinate[] dfgfdfgh = geometry.getCoordinates();
         Coordinate[] fdsfgdfg = curveBuilder.getLineCurve(dfgfdfgh, 1);
         Geometry dfgdfg = new GeometryFactory().createPolygon(fdsfgdfg);
+    }
+
+    public static void test_collection() throws ParseException {
+        Geometry point = new WKTReader().read("POINT (-1 -1)");
+        Geometry lineString = new WKTReader().read("LINESTRING (0 0, 10 10, 20 20)");
+        Geometry polygon = new WKTReader().read("POLYGON ((1 1,2 1,2 2,2 1,1 1))");
+        Geometry multiPolygon = new WKTReader().read("MULTIPOLYGON ( ((0 0, 1 0, 1 1, 0 1, 0 0)) )");
+        Geometry[] geometries = {point, lineString};
+        ArrayList<Geometry> geometriesList = new ArrayList<Geometry>(Arrays.asList(geometries));
+        GeometryCollection geometryCollection = new GeometryFactory().createGeometryCollection(geometries);
+        Double gfgh = geometryCollection.getCoordinates()[0].x;
+        String s = geometryCollection.getGeometryType();
+        geometryCollection = new GeometryFactory().createGeometryCollection();
+        Geometry g1 = new WKTReader().read("POLYGON ((1 1,2 1,2 2,1 2,1 1))");
+        Geometry ggggggg = new WKTReader().read("POLYGON EMPTY");
+        Geometry aaaaaaa = new WKTReader().read("POINT (-1 -1)");
+        Geometry asdf = new WKTReader().read("MULTIPOLYGON (((0 0, 1 0, 1 1, 0 1, 0 0)))");
+        Geometry g2 = new WKTReader().read("MULTIPOLYGON (((0 0, 0 1, 1 1, 1 0, 0 0)), ((10 10, 10 20, 20 20, 20 10, 10 10)))");
+        Geometry g3 = g1.union(g2);
+        String ssfdgfg = geometryCollection.getGeometryType();
+        String asffdsgdfgfg = asdf.getGeometryType();
+        Geometry dfdgdfghfgh = ggggggg.union(aaaaaaa);
+        String dfgfgh = asdf.getGeometryType();
+
+//        MultiPolygon dfghdfgh = (MultiPolygon) new WKTReader().read("multipolygon (((0 0, 10 0, 10 10, 0 10, 0 0), (11 11, 20 11, 20 20, 20 11, 11 11)))");
+        MultiPolygon dfghdfgh = (MultiPolygon) new WKTReader().read("MULTIPOLYGON (((0 0, 10 0, 10 10, 0 10, 0 0)), ((11 11, 20 11, 20 20, 20 11, 11 11)))");
+        Geometry gdfgdfh = dfghdfgh.getEnvelope();
+        String fghg = "fsdgdfhdfh";
+        double df = java.lang.Math.pow(10, 3);
+        GeometryCollection dfgdfg = new org.locationtech.jts.geom.GeometryFactory().createGeometryCollection();
     }
 
     public static void main(String[] args) throws ParseException {
@@ -54,7 +121,7 @@ public class TestJavaJTS {
 
         Boolean isvalidop = geometry != null && new IsValidOp(geometry).isValid();
 
-        System.out.println(isvalidop);
+//        System.out.println(isvalidop);
 
         Boolean isSimple = geometry != null && geometry.isSimple();
 
@@ -71,7 +138,8 @@ public class TestJavaJTS {
         PrecisionModel sddf = new PrecisionModel(34);
         Geometry stedrytr = org.locationtech.jts.precision.GeometryPrecisionReducer.reduce(geometry, new org.locationtech.jts.geom.PrecisionModel(34));
 
-        Geometry geometry1 =  new WKTReader().read("POLYGON EMPTY");;
+        Geometry geometry1 = new WKTReader().read("POLYGON EMPTY");
+        ;
 
         Geometry inter = new WKTReader().read("POINT (50 50)").intersection(new WKTReader().read("POLYGON ((0 0, 40 0, 40 40, 0 40, 0 0))"));
 
@@ -88,7 +156,6 @@ public class TestJavaJTS {
 //        Geometry gdfgdfhfgh = JTS.Trans
 
 
-
         Geometry point = new WKTReader().read("POINT (1 1)");
 
         String fssdfg = point.getGeometryType();
@@ -100,23 +167,61 @@ public class TestJavaJTS {
         double tolat = point.getInteriorPoint().getY();
         double fromlon = point.getInteriorPoint().getX();
         double tolon = point.getInteriorPoint().getX();
-double latitudeArc = (fromlat - tolat) * 0.017453292519943295769236907684886;
-double longitudeArc = (fromlon - tolon) * 0.017453292519943295769236907684886;
-double latitudeH = java.lang.Math.sin(latitudeArc * 0.5);
-latitudeH *= latitudeH;
-double lontitudeH = java.lang.Math.sin(longitudeArc * 0.5);
-lontitudeH *= lontitudeH;
-double tmp = java.lang.Math.cos(fromlat * 0.017453292519943295769236907684886) *
-        java.lang.Math.cos(tolat * 0.017453292519943295769236907684886);
-double res =  6372797.560856 * (2.0 * java.lang.Math.asin(java.lang.Math.sqrt(latitudeH + tmp * lontitudeH)));
+        double latitudeArc = (fromlat - tolat) * 0.017453292519943295769236907684886;
+        double longitudeArc = (fromlon - tolon) * 0.017453292519943295769236907684886;
+        double latitudeH = java.lang.Math.sin(latitudeArc * 0.5);
+        latitudeH *= latitudeH;
+        double lontitudeH = java.lang.Math.sin(longitudeArc * 0.5);
+        lontitudeH *= lontitudeH;
+        double tmp = java.lang.Math.cos(fromlat * 0.017453292519943295769236907684886) *
+                java.lang.Math.cos(tolat * 0.017453292519943295769236907684886);
+        double res = 6372797.560856 * (2.0 * java.lang.Math.asin(java.lang.Math.sqrt(latitudeH + tmp * lontitudeH)));
 
-if ((fromlat > 180) || (fromlat < -180) || (fromlon > 90) || (fromlon < -90) ||
-        (tolat > 180) || (tolat < -180) || (tolon > 90) || (tolon < -90)) {
+        if ((fromlat > 180) || (fromlat < -180) || (fromlon > 90) || (fromlon < -90) ||
+                (tolat > 180) || (tolat < -180) || (tolon > 90) || (tolon < -90)) {
 
-}
+        }
 
+//        test_curve();
+//        test_geotools_curve();
+        test_collection();
+//        AffineTransformation affineTransformation = new AffineTransformation();
+//
+//        affineTransformation = affineTransformation.translate(-5, 24);
+//        System.out.println(geometry.getCoordinates()[0]);
+        Geometry fgfg = new org.locationtech.jts.geom.util.AffineTransformation().translate(-5, 24).transform(geometry);
+//        System.out.println(fgfg.getCoordinates()[0]);
 
-        test_curve();
+        Geometry rotate1 = new WKTReader().read("LINESTRING (0 0,5 0)");
+
+        Geometry dfgfhdfa = new org.locationtech.jts.geom.util.AffineTransformation().rotate(2 * acos(0.0), 1, 0).transform(rotate1);
+
+        double scalaX = rotate1.getEnvelopeInternal().centre().x;
+        double scalaY = rotate1.getEnvelopeInternal().centre().y;
+        Geometry fdfgfhfjf = org.locationtech.jts.geom.util.AffineTransformation.scaleInstance(2, 2, scalaX, scalaY).transform(rotate1);
+
+        System.out.println(dfgfhdfa.getCoordinates()[0]);
+
+        geometry.symDifference(geometry);
+
+        geometry.difference(geometry);
+
+        geometry.union(geometry);
+
+        geometry.disjoint(geometry);
+
+        geometry.isEmpty();
+
+        geometry.getBoundary();
+
+        Geometry geometry2 = new WKTReader().read("POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))");
+        Geometry polygon = geometry2.getGeometryType().equals("Polygon") ? geometry2 : new org.locationtech.jts.geom.GeometryFactory().createPolygon(geometry2.getCoordinates()).getExteriorRing();
+
+        p.getExteriorRing();
+
+        Geometry dfgfhfghj = new org.locationtech.jts.geom.util.AffineTransformation().scale(1, 0).transform(rotate1);
+
+        Geometry fsvertdg = new org.locationtech.jts.geom.util.AffineTransformation(2, 2, 2, 2, 2, 2).transform(rotate1);
 
         System.out.println("dsfdsgdfgdfghdf");
     }
